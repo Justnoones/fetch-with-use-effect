e7deb63cd0147cbf7e909e516c0f14a3d6e1596a
Justnoones <yanw97841@gmail.com>
2023-08-24 08:20:33 +0630

    use-effect's dependency with filter feature

--- src/components/TripList/index.js
+++ src/components/TripList/index.js
@@ -4,22 +4,20 @@ import { useEffect, useState } from 'react';
 
 export default function TripList() {
   let [trips, setTrips] = useState([]);
+  let [url, setUrl] = useState("http://localhost:3001/trip");
   useEffect(() => {
-    fetch("http://localhost:3001/trip")
+    fetch(url)
       .then(res => res.json())
       .then(data => {
         setTrips(data);
       });
-  }, []);
-  console.log(trips);
-  // let [trip, setTrip] = useState([]);
-  // fetch("http://localhost:3001/trip")
-  // .then(res => res.json)
-  // .then(data => {
-  //   console.log(data);
-  // });
+  }, [url]);
   return (
     <div>
+      <h1>Ready To Go?</h1>
+      <button onClick={_ => setUrl("http://localhost:3001/trip")}>All</button>
+      <button onClick={_ => setUrl("http://localhost:3001/trip?location=Myanmar")}>Trips in Myanmar</button>
+      <button onClick={_ => setUrl("http://localhost:3001/trip?location=Thailand")}>Trips in Thailand</button>
       <ul>
         {trips.map(trip => (
         <li key={trip.id}>
